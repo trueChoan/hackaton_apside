85ebaa185177b842de28c22a846b0aa598634528
Benoît V <benoit@vandanjon.fr>
2022-06-30 00:47:04 +0200

    cards setup + resize max navselector

--- assets/components/TableCards.jsx
+++ assets/components/TableCards.jsx
@@ -11,13 +11,20 @@ const TableCards = () => {
 				<span>1 | 2 | 3 | ...</span>
 			</article>
 			<article className="cards-container">
-				<Card mission="orange" />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
+				<Card bgColor="orange" location="Nantes" />
+				<Card bgColor="orange" location="Paris" />
+				<Card bgColor="green" location="Nantes" />
+				<Card bgColor="yellow" location="Nantes" />
+				<Card bgColor="blue" location="Paris" />
+				<Card bgColor="blue" location="Lisbon" />
+				<Card bgColor="green" location="Lisbon" />
+				<Card bgColor="orange" location="Nantes" />
+				<Card bgColor="orange" location="Vienne" />
+				<Card bgColor="green" location="Le Tampon" />
+				<Card bgColor="yellow" location="Nantes" />
+				<Card bgColor="blue" location="Nantes" />
+				<Card bgColor="blue" location="Berlin" />
+				<Card bgColor="green" location="Nantes" />
 			</article>
 		</section>
 	);

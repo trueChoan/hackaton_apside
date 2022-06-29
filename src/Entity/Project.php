5853791259452bd5b09a11917a8916bfbcb26b90
Mickael Desbiens <desbiens.mickael01@gmail.com>
2022-06-29 18:29:40 +0200

    Creation of a project and resource entity

---
+++ src/Entity/Project.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\ProjectRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProjectRepository::class)]
+#[ApiResource]
+class Project
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 45)]
+    private $name;
+
+    #[ORM\Column(type: 'text')]
+    private $description;
+
+    #[ORM\OneToMany(mappedBy: 'project', targetEntity: Ressource::class)]
+    private $ressource;
+
+    public function __construct()
+    {
+        $this->ressource = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Ressource>
+     */
+    public function getRessource(): Collection
+    {
+        return $this->ressource;
+    }
+
+    public function addRessource(Ressource $ressource): self
+    {
+        if (!$this->ressource->contains($ressource)) {
+            $this->ressource[] = $ressource;
+            $ressource->setProject($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRessource(Ressource $ressource): self
+    {
+        if ($this->ressource->removeElement($ressource)) {
+            // set the owning side to null (unless already changed)
+            if ($ressource->getProject() === $this) {
+                $ressource->setProject(null);
+            }
+        }
+
+        return $this;
+    }
+}

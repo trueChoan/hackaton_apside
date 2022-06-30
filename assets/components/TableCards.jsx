049eb95dfe691835139ec2e5669dc2e48b2ed111
Saucy Corentin <saucy.corentin@gmail.com>
2022-06-30 15:55:20 +0200

    Mise à jour diverses

--- assets/components/TableCards.jsx
+++ assets/components/TableCards.jsx
@@ -14,20 +14,20 @@ const TableCards = () => {
 			<ButtonPage />
 			</article>
 			<article className="cards-container">
-				<Card bgColor="orange" location="Nantes" collab={19} />
-				<Card bgColor="orange" location="Paris" collab={17} />
-				<Card bgColor="green" location="Nantes" collab={15} />
-				<Card bgColor="yellow" location="Nantes" collab={15} />
-				<Card bgColor="blue" location="Paris" collab={10} />
-				<Card bgColor="blue" location="Lisbon" collab={25} />
-				<Card bgColor="green" location="Lisbon" collab={54} />
-				<Card bgColor="orange" location="Nantes" collab={12} />
-				<Card bgColor="orange" location="Vienne" collab={14} />
-				<Card bgColor="green" location="Le Tampon" collab={19} />
-				<Card bgColor="yellow" location="Nantes" collab={29} />
-				<Card bgColor="blue" location="Nantes" collab={16} />
-				<Card bgColor="blue" location="Berlin" collab={37} />
-				<Card bgColor="green" location="Nantes" collab={39} />
+				<Card bgColor="orange" location="Nantes 🇫🇷" collab={19} />
+				<Card bgColor="orange" location="London 🇫🇷" collab={17} />
+				<Card bgColor="green" location="Nantes 🇫🇷 " collab={15} />
+				<Card bgColor="yellow" location="USA 🇫🇷" collab={15} />
+				<Card bgColor="blue" location="Paris 🇫🇷" collab={10} />
+				<Card bgColor="blue" location="Lisbon 🇵🇹" collab={25} />
+				<Card bgColor="green" location="Lisbon 🇵🇹" collab={54} />
+				<Card bgColor="orange" location="Madrid 🇪🇸" collab={12} />
+				<Card bgColor="orange" location="Vienne 🇦🇹" collab={14} />
+				<Card bgColor="green" location="Paris 🇫🇷" collab={19} />
+				<Card bgColor="yellow" location="Bulgarie 🇧🇬" collab={29} />
+				<Card bgColor="blue" location="Nantes 🇫🇷" collab={16} />
+				<Card bgColor="blue" location="Berlin 🇩🇪" collab={37} />
+				<Card bgColor="green" location="Nantes 🇫🇷" collab={39} />
 			</article>
 		</section>
 	);

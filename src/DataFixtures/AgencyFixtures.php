d05d6d2b8cbfef5c8e7fd0917b289f93461265ee
unknown <ala.johan@gmail.com>
2022-06-30 18:39:14 +0200

    add flag and name custom name and custom description

--- src/DataFixtures/AgencyFixtures.php
+++ src/DataFixtures/AgencyFixtures.php
@@ -10,17 +10,17 @@ use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 class AgencyFixtures extends Fixture
 {
     public const AGENCIES = [
-        ['name' => 'Paris'],
-        ['name' => 'Lille'],
-        ['name' => 'Bruxelles'],
-        ['name' => 'Nice'],
-        ['name' => 'Tours'],
-        ['name' => 'Lyon'],
-        ['name' => 'Vernon'],
-        ['name' => 'Nantes'],
-        ['name' => 'Aix en Provence'],
-        ['name' => 'Casablanca'],
-        ['name' => 'Genève'],
+        ['name' => 'Paris', 'flag' => 'france.png'],
+        ['name' => 'Lille', 'flag' => 'france.png'],
+        ['name' => 'Bruxelles', 'flag' => 'belgique.png'],
+        ['name' => 'Nice', 'flag' => 'france.png'],
+        ['name' => 'Tours', 'flag' => 'france.png'],
+        ['name' => 'Lyon', 'flag' => 'france.png'],
+        ['name' => 'Vernon', 'flag' => 'france.png'],
+        ['name' => 'Nantes', 'flag' => 'france.png'],
+        ['name' => 'Aix en Provence', 'flag' => 'france.png'],
+        ['name' => 'Casablanca', 'flag' => 'maroc.png'],
+        ['name' => 'Genève', 'flag' => 'suisse.png'],
 
     ];
 
@@ -30,6 +30,7 @@ class AgencyFixtures extends Fixture
         foreach (self::AGENCIES as $key => $location) {
             $agency = new Agency();
             $agency->setName($location['name']);
+            $agency->setFlag($location['flag']);
             $manager->persist($agency);
             $this->addReference('agency_' . $key, $agency);
         }
